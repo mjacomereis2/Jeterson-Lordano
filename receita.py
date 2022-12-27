a31c98e2550e04aa452247e570032d7ef57387b8
marcelo reis <mjacomereis2@gmail.com>
2022-12-27 10:59:44 -0200

    Primeiro commit

---
+++ receita.py
@@ -0,0 +1,15 @@
+3 cenouras médias
+4 ovos
+1 xícara e 1/2 (chá) de óle2 xícaras (chá) de açúcar
+3 xícara e 1/4 (chá) Farinha de Trigo Traal Renata
+1 xícara (sopa) de fermento em pó
+Calda:
+1 xícara (chá) de chocolate em pó
+1 xícara (chá) de açú
+1 colher (sopa) de mateiga
+½ xícara (chá) de leite 
+
+Modo de preparo
+
+
+Levar ao forno por 30 minutos.
